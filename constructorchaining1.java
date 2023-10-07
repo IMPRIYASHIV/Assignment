f3d48358b23eb0a521988eb1f4d6105ad325396c
PriyaShiv <impriyariya7@gmail.com>
2023-10-07 04:26:42 +0000

    Checking whether const.chain1. of java can be added with different value after merge

--- constructorchaining1.java
+++ constructorchaining1.java
@@ -11,7 +11,7 @@ this.(brand);
 this.cost=cost;
 }
 public static void main(String args[]) {
-Mobile m = new Mobile("SamsungsA30",50000);
+Mobile m = new Mobile("SamsungsA30",10000);
 System.out.println("Brand: "+m.brand);
 System.out.println("Cost: "+m.cost);
 }
